d27cdb57f6c16bc19792be37b57f487c7b4169f8
Anton Savranskiy <47571615+sm011a@users.noreply.github.com>
2021-02-10 16:42:43 +0300

    Add player

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -11,13 +11,19 @@ namespace SA
 
         public Color greenLight;
         public Color greenDark;
+        public Color playerColor = Color.black;
+
+        GameObject playerObj;
 
         private GameObject mapObject;
         private SpriteRenderer mapRenderer;
 
+        Node[,] grid;
+
         private void Start()
         {
             CreateMap();
+            PlacePlayer();
         }
 
         private void CreateMap()
@@ -25,11 +31,26 @@ namespace SA
             mapObject = new GameObject("Map");
             mapRenderer = mapObject.AddComponent<SpriteRenderer>();
 
+            grid = new Node[maxWidthMap, maxHeigtMap];
+
             Texture2D texture = new Texture2D(maxWidthMap, maxHeigtMap);
             for (int x = 0; x < maxWidthMap; x++)
             {
                 for (int y = 0; y < maxHeigtMap; y++)
                 {
+                    Vector3 tp = Vector3.zero;
+                    tp.x = x;
+                    tp.y = y;
+
+                    Node n = new Node()
+                    {
+                        x = x,
+                        y = y,
+                        worldPosition = tp
+                    };
+
+                    grid[x, y] = n;
+
                     #region Visual
                     if (x % 2 != 0)
                     {
@@ -64,5 +85,32 @@ namespace SA
             mapRenderer.sprite = sprite;
         }
 
+        private void PlacePlayer()
+        {
+            playerObj = new GameObject("Player");
+            SpriteRenderer playerRender = playerObj.AddComponent<SpriteRenderer>();
+            playerRender.sprite = CreateSprite(playerColor);
+            playerRender.sortingOrder = 1;
+
+            playerObj.transform.position = GetNode(3, 3).worldPosition;
+        }
+
+        Node GetNode(int x, int y)
+        {
+            if (x < 0 || x > maxWidthMap - 1 || y < 0 || y > maxHeigtMap - 1)
+                return null;
+
+            return grid[x, y];
+        }
+
+        Sprite CreateSprite(Color targetColor)
+        {
+            Texture2D texture = new Texture2D(1, 1);
+            texture.SetPixel(0, 0, targetColor);
+            texture.Apply();
+            texture.filterMode = FilterMode.Point;
+            Rect rect = new Rect(0, 0, 1, 1);
+            return Sprite.Create(texture, rect, Vector2.one * 0.5f, 1, 0, SpriteMeshType.FullRect);
+        }
     }
 }
---
+++ New Unity Project/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class Node : MonoBehaviour
+    {
+        public int x;
+        public int y;
+        public Vector3 worldPosition;
+    }
+}
